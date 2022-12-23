ee3ecdf9bae3355c75c557d86464d6d9087dc174
ulascc <ulas.can2424@gmail.com>
2022-12-23 12:11:39 +0300

    git soap service

---
+++ NewPasswordSoap/NewPasswordCreatorService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="NewPasswordCreatorService.asmx.cs" Class="NewPasswordSoap.NewPasswordCreatorService" %>
---
+++ NewPasswordSoap/NewPasswordCreatorService.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace NewPasswordSoap
+{
+    /// <summary>
+    /// Summary description for NewPasswordCreatorService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class NewPasswordCreatorService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string NewPasswordCreator()
+        {
+            Random random = new Random();
+            int sifreKodu = random.Next(999999);
+            string sifreKod = sifreKodu.ToString();
+
+            return sifreKod;
+        }
+    }
+}
